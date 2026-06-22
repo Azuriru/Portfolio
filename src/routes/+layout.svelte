4df915851a93a78c14d5f1f38c0d95eefce5f86c
Azuriru <kegumine@gmail.com>
2026-06-23 04:39:19 +0800

    feat: test meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
+    import { updateByIndex } from '$lib/util/array';
     import '../app.css';
     import './+layout.scss';
 </script>
 
 <svelte:head>
     <title>Azuriru</title>
+    <meta name="title" content="Azuriru's Portfolio" />
+    <meta name="description" content="There is a only finite amount of ways to screw something up, so eventually, you'll succeed." />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://azuriru.vercel.app" />
+    <meta property="og:title" content="Azuriru" />
+    <meta property="og:description" content="There is a only finite amount of ways to screw something up, so eventually, you'll succeed." />
+    <meta property="og:image" content="./assets/buntini_logo.png" />
+
+    <meta property="og:site_name" content="Azuriru" />
+    <meta property="og:locale" content="en_US" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:url" content="https://azuriru.vercel.app" />
+    <meta name="twitter:title" content="Azuriru" />
+    <meta name="twitter:description" content="There is a only finite amount of ways to screw something up, so eventually, you'll succeed." />
+    <meta name="twitter:image" content="./assets/buntini_logo.png" />
 </svelte:head>
 
 <div class="app">
